3efb7f1e355dbde8cff5a76a4dea28ac13a082e2
Nicolay Dammer <nicolay.dammer@hr4you.de>
2020-02-27 16:11:40 +0100

    snipeCalculator Finished

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+function minutes_with_leading_zeros(dt) {
+    return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+}
+
+function stringToDateTimeObject(string, boolean) {
+    let dateObject = new Date();
+    let splittedString = string.split(' ');
+    let count = 0;
+    let splitTimeString;
+    if (splittedString.length > 1) {
+        splitTimeString = splittedString[2].split(':')
+    } else {
+        splitTimeString = string.split(':');
+    }
+    if (splittedString[0] === 'morgen') {
+        dateObject.setDate(dateObject.getDate()+1)
+    }
+    dateObject.setHours(splitTimeString[0]);
+    dateObject.setMinutes(splitTimeString[1]);
+    dateObject.setSeconds(splitTimeString[2]);
+    if (boolean === false) {
+        dateObject.setMilliseconds(0);
+    } else {
+        dateObject.setMilliseconds(splitTimeString[3]);
+    }
+    return dateObject;
+}
+
+function calculateRetreatTime(troepen_aankomst, troepen_aankomst_snipe) {
+    if (troepen_aankomst.getDate() !== troepen_aankomst_snipe.getDate()) {
+        troepen_aankomst_snipe.setDate(troepen_aankomst_snipe.getDate() + 1)
+    }
+    troepen_aankomst_snipe.setHours(troepen_aankomst_snipe.getHours() - troepen_aankomst.getHours());
+    troepen_aankomst_snipe.setMinutes(troepen_aankomst_snipe.getMinutes() - troepen_aankomst.getMinutes());
+    troepen_aankomst_snipe.setSeconds(troepen_aankomst_snipe.getSeconds() - troepen_aankomst.getSeconds());
+    troepen_aankomst_snipe.setMilliseconds(troepen_aankomst_snipe.getMilliseconds() - troepen_aankomst.getMilliseconds() + 50);
+    return troepen_aankomst_snipe
+}
+
+function calculateAttackTimeWithOffset(troepen_aankomst, troepen_aankomst_snipe) {
+    if (troepen_aankomst.getDate() !== troepen_aankomst_snipe.getDate()) {
+        troepen_aankomst_snipe.setDate(troepen_aankomst_snipe.getDate())
+    }
+    troepen_aankomst_snipe.setHours(troepen_aankomst_snipe.getHours() - troepen_aankomst.getHours());
+    troepen_aankomst_snipe.setMinutes(troepen_aankomst_snipe.getMinutes() + 2 - troepen_aankomst.getMinutes());
+    troepen_aankomst_snipe.setSeconds(troepen_aankomst_snipe.getSeconds() - troepen_aankomst.getSeconds());
+    troepen_aankomst_snipe.setMilliseconds(troepen_aankomst_snipe.getMilliseconds() - troepen_aankomst.getMilliseconds() + 25);
+    return troepen_aankomst_snipe
+}
+
 $(document).ready(function () {
     $('#verdedigende_snipe_checkbox').change(function () {
         if (this.checked) {
@@ -12,16 +62,30 @@ $(document).ready(function () {
 
     $('#snipe_calculator_form').on('submit', function (e) {
         e.preventDefault();
-        let troepen_aankomst = $('#aankomst_dorp');
-        let troepen_aankomst_snipe = $('#troepen_aankomst_snipe');
+        let troepen_aankomst = stringToDateTimeObject($('#aankomst_dorp').val(), false);
+        let troepen_aankomst_snipe = stringToDateTimeObject($('#troepen_aankomst_snipe').val(), true);
         let aanvals_tijd = $('#aanvals_tijd');
-
-        if ($('#verdedigende_snipe_checkbox:checked')) {
-            let terugtrek_tijd = $('#terugtrek_tijd');
-            //todo: calculate defending time
-        }else {
-            //todo: calculate attacking time
+        let aanvals_tijd_calculated;
+        if ($('#verdedigende_snipe_checkbox').prop('checked')) {
+            let terugtrek_tijd = calculateRetreatTime(troepen_aankomst, troepen_aankomst_snipe);
+            $('#terugtrek_tijd').html(
+                terugtrek_tijd.getDate() +
+                '-' + (terugtrek_tijd.getMonth() + 1) +
+                '-' + terugtrek_tijd.getFullYear() +
+                ' ' + terugtrek_tijd.getHours() +
+                ':' + minutes_with_leading_zeros(terugtrek_tijd) +
+                ':' + terugtrek_tijd.getSeconds() +
+                ':' + terugtrek_tijd.getMilliseconds());
+            aanvals_tijd_calculated = calculateAttackTimeWithOffset(troepen_aankomst, troepen_aankomst_snipe);
+        } else {
+            aanvals_tijd_calculated = calculateRetreatTime(troepen_aankomst, troepen_aankomst_snipe);
         }
+        aanvals_tijd.html(aanvals_tijd_calculated.getDate() +
+            '-' + (aanvals_tijd_calculated.getMonth() + 1) +
+            '-' + aanvals_tijd_calculated.getFullYear() +
+            ' ' + aanvals_tijd_calculated.getHours() +
+            ':' + minutes_with_leading_zeros(aanvals_tijd_calculated) +
+            ':' + aanvals_tijd_calculated.getSeconds() +
+            ':' + aanvals_tijd_calculated.getMilliseconds());
     });
 });
-
